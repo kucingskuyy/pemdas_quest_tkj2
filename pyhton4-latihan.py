ddaf4af62bbdf91ff2415a4c7b0f9d6d56da38dc
kucingskuyy <111480071+kucingskuyy@users.noreply.github.com>
2022-08-21 23:36:12 -0700

    Add files via upload

---
+++ pyhton4-latihan.py
@@ -0,0 +1,21 @@
+print("==================")
+print("rumus bangun datar")
+print("==================")
+
+print("1.persegi\n")
+
+sisi = int(input("masukan nilai sisi: "))
+luas_p = sisi * sisi
+print("luas persegi adalah ", luas_p)
+
+print("\n2 persegi panjang")
+
+
+alas = int(input("masukan nilai alas: "))
+tinggi =int(input("masukan nilai tinggi: "))
+luas_s = alas * tinggi / 2 
+print("luas segitiga adalah: ", luas_s)
+
+
+
+
